4e08d5f0e2aee6e7e835f7349c5ea188e6c4f029
AifaYes <41337038+AifaYes@users.noreply.github.com>
2024-12-24 15:37:26 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,15 @@
 <?php
-    $id= $_POST['id']
-    $name= $_POST['name']
-    $Dest= $_POST['Dest']
-    $budget= $_POST['budget']
-    $conn= new my sqli('localhost','root','test');
-    if($conn->connect_error){
-        die('Connection Failed : '.$conn->connect_error);
-    }else{
-        $stmt= $conn->prepare("insert into registration(id,name,Dest,budget)values(?,?,?,?)");
-        $stmt->bind_param("issd",$id,$name,$Dest,$budget);
-        $stmt->execute();
-        $stmt->close();
-        $conn->close();
+    $id= $_POST['id'];
+    $name= $_POST['name'];
+    $Dest= $_POST['Dest'];
+    $budget= $_POST['budget'];
+    $file="../C/data.txt";
+    $handle = fopen($file, 'w'); 
+
+    if ($handle) {
+        $data = "$id\n$name\n$Dest\n$budget\n";
+        fwrite($handle, $data);
+        fclose($handle);
+        echo "success";
     }
 ?>
